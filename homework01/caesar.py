804f0f7fa88285a30f53f4835bb6103f7ad8c7e8
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2021-11-18 14:19:14 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,17 +2,6 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     chiphertext = ""
     for i, _ in enumerate(plaintext):
         if plaintext[i].isalpha():
@@ -29,17 +18,6 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
 
 def decrypt_caesar(chiphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a chiphertext using a Caesar cipher.
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
     for i, _ in enumerate(chiphertext):
         if chiphertext[i].isalpha():
