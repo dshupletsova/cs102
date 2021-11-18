1508d95cad280e35b52595d6b2217bc21721a437
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2021-11-18 14:17:01 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -76,7 +76,6 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
     plain = [chr((char ** key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
     return "".join(plain)
 
 
