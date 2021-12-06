94d89db4338e848364f895da4fc08434103d673e
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2021-12-06 23:10:14 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -115,7 +115,7 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     >>> values == {'2', '5', '9'}
     True
     """
-    values = []
+    values: List[int] = []
     row = get_row(grid, pos)
     col = get_col(grid, pos)
     block = get_block(grid, pos)
@@ -127,7 +127,7 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     """Решение пазла, заданного в grid"""
-    """Как решать Судоку?
+    """ Как решать Судоку?
         1. Найти свободную позицию
         2. Найти все возможные значения, которые могут находиться на этой позиции
         3. Для каждого возможного значения:
@@ -138,16 +138,14 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
     pos = find_empty_positions(grid)
-    if pos == (None, None):
+    if not pos:
         return grid
-    else:
-        n, m = find_empty_positions(grid)
-        ans = find_possible_values(grid, pos)
-        for i in ans:
-            grid[n][m] = str(i)
-            if solve(grid) is not None:
-                return grid
-            grid[n][m] = "."
+    possible_values = find_possible_values(grid, pos)
+    for i in possible_values:
+        grid[pos[0]][pos[1]] = i
+        if solve(grid):
+            return solve(grid)
+    grid[pos[0]][pos[1]] = "."
     return None
 
 
