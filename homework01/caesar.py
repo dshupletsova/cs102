f1b593aa53c90daae1d416ac48aead622562359b
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2022-01-21 13:45:26 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,7 +2,7 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    chiphertext = ""
+    ciphertext = ""
     for letter in plaintext:
         if letter.isalpha():
             if letter.islower():
@@ -17,7 +17,7 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     return ciphertext
 
 
-def decrypt_caesar(chiphertext: str, shift: int = 3) -> str:
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
     for letter in ciphertext:
         if letter.isalpha():
@@ -33,9 +33,6 @@ def decrypt_caesar(chiphertext: str, shift: int = 3) -> str:
     return plaintext
 
 
-def caesar_breaker_brute_force(chiphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
+def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     return best_shift
