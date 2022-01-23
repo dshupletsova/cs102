b03e4af0d6c83f39ca955c1996e5e49c6eab3e95
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2022-01-23 14:06:38 +0300

    Update life.py

--- homework03/life.py
+++ homework03/life.py
@@ -12,63 +12,91 @@ Grid = tp.List[Cells]
 
 class GameOfLife:
     def __init__(
-        self,
-        size: tp.Tuple[int, int],
-        randomize: bool = True,
-        max_generations: tp.Optional[float] = float("inf"),
+        self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
     ) -> None:
-        # Размер клеточного поля
-        self.rows, self.cols = size
-        # Предыдущее поколение клеток
-        self.prev_generation = self.create_grid()
-        # Текущее поколение клеток
-        self.curr_generation = self.create_grid(randomize=randomize)
-        # Максимальное число поколений
-        self.max_generations = max_generations
-        # Текущее число поколений
-        self.generations = 1
+        self.width = width
+        self.height = height
+        self.cell_size = cell_size
+        self.screen_size = width, height
+        self.screen = pygame.display.set_mode(self.screen_size)
+        self.cell_width = self.width // self.cell_size
+        self.cell_height = self.height // self.cell_size
+        self.speed = speed
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        if randomize:
+            grid = [[random.randint(0, 1) for i in range(self.cols)] for i in range(self.rows)]
+        else:
+            grid = [[0 for i in range(self.cols)] for i in range(self.rows)]
+        return grid
 
-    def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+    def get_neighbours(self, cell: Cell):
+        new_list = []
+        y, x = cell[0], cell[1]
+        if 0 < y:
+            new_list.append(self.grid[y - 1][x])
+        if y < self.cell_height - 1:
+            new_list.append(self.grid[y + 1][x])
+        if x > 0:
+            new_list.append(self.grid[y][x - 1])
+        if x < self.cell_width - 1:
+            new_list.append(self.grid[y][x + 1])
+        if y > 0 and x > 0:
+            new_list.append(self.grid[y - 1][x - 1])
+        if y > 0 and x < self.cell_width - 1:
+            new_list.append(self.grid[y - 1][x + 1])
+        if y < self.cell_height - 1 and x > 0:
+            new_list.append(self.grid[y + 1][x - 1])
+        if y < self.cell_height - 1 and x < self.cell_width - 1:
+            new_list.append(self.grid[y + 1][x + 1])
+        return new_list
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        new_grid = [[0 for i in range(self.cell_width)] for j in range(self.cell_height)]
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                neighbours = self.get_neighbours((i, j))
+                if self.grid[i][j] == 0:
+                    if sum(neighbours) == 3:
+                        new_grid[i][j] = 1
+                    else:
+                        new_grid[i][j] = 0
+                else:
+                    if sum(neighbours) < 2 or sum(neighbours) > 3:
+                        new_grid[i][j] = 0
+                    else:
+                        new_grid[i][j] = 1
+        return new_grid
 
     def step(self) -> None:
-        """
-        Выполнить один шаг игры.
-        """
-        pass
+        self.prev_generation = self.curr_generation
+        self.curr_generation = self.get_next_generation()
+        self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
-        """
-        Не превысило ли текущее число поколений максимально допустимое.
-        """
-        pass
+        if self.max_generations and self.generations >= self.max_generations:
+            return True
+        return False
 
     @property
     def is_changing(self) -> bool:
-        """
-        Изменилось ли состояние клеток с предыдущего шага.
-        """
-        pass
+        if self.prev_generation != self.curr_generation:
+            return True
+        return False
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
-        """
-        Прочитать состояние клеток из указанного файла.
-        """
-        pass
+        with open(filename) as inp:
+            grid = [[int(cell) for cell in row.strip()] for row in inp]
+        game = GameOfLife((len(grid), len(grid[0])))
+        game.curr_generation = grid
+        return game
 
     def save(self, filename: pathlib.Path) -> None:
-        """
-        Сохранить текущее состояние клеток в указанный файл.
-        """
-        pass
+        with open(filename, "w") as outp:
+            for i in self.curr_generation:
+                stroka = ""
+                for j in i:
+                    stroka += str(j)
+                outp.write(stroka + "\n")
