c3ab32b77e8c2649ed4ff903c13955dd86f2b004
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2022-04-14 16:16:19 +0300

    Update config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
-    "version": "5.126",
+    "access_token": "755e74dd73144274613739738c1cbd998c1135f765b239a12c0aa98dec93ec3067c24f85adcd8dca03fb1",
+    "version": "5.131",
 }
