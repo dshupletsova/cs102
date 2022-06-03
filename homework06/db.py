ef6bb083a87cbc03136b46d8d40c9ece277e80fa
darya shupletsova <90191344+dshupletsova@users.noreply.github.com>
2022-06-04 01:58:45 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,4 +1,3 @@
- scraputils import get_news
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,10 +5,24 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
-Base.metadata.create_all(bind=engine)
+from homework06.scraputils import get_news
 
 
-class News(Base):
+def create_db(news_list):
+    s = session()
+    for dic in news_list:
+        new = News(
+            title=dic["title"],
+            author=dic["author"],
+            url=dic["url"],
+            comments=dic["comments"],
+            points=dic["points"],
+        )
+        s.add(new)
+        s.commit()
+
+
+class News(Base):  # type: ignore
     __tablename__ = "news"
     id = Column(Integer, primary_key=True)
     title = Column(String)
@@ -20,5 +33,9 @@ class News(Base):
     label = Column(String)
 
 
-s = session()
+Base.metadata.create_all(bind=engine)
 
+if __name__ == "__main__":
+
+    the_list = get_news("https://news.ycombinator.com/", n_pages=34)
+    create_db(the_list)
